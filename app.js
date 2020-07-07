073a101d14464eeb9b9e58a38b8621c65cc72343
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-06 22:49:29 -0400

    javascriptVarPractice

---
+++ app.js
@@ -0,0 +1,55 @@
+var a;
+a = 1;
+
+var b;
+b = 2;
+
+var c;
+c = 3;
+
+var d;
+d = 4;
+
+var e;
+e = 5;
+
+var f;
+f = 6;
+
+var g;
+g = 7;
+var h;
+h = 8;
+var i;
+i = 9
+
+var j;
+j = 10;
+
+        console.log(a+b);
+
+        console.log(c+d);
+
+        console.log(c-b);
+
+        console.log(b-c);
+
+        console.log(e*f);
+
+        console.log(f*e);
+
+        console.log(f/g);
+
+        console.log(g/f);
+
+        console.log(a++);
+
+        console.log(b++);
+
+        console.log(f--);
+
+        console.log(h--);
+        
+
+
+
